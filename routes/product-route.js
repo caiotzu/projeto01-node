56a54d096d74526c04c9342148d52f01153e5810
Caio Costa <caio.costa.ads@gmail.com>
2020-09-12 15:48:19 -0300

    crud

--- routes/product-route.js
+++ routes/product-route.js
@@ -21,7 +21,7 @@ router.post('/', function(req, res) {
     });
 });
 
-// Get=> localhost:3000/api/produtos
+// GetAll=> localhost:3000/api/produtos
 router.get('/', function(req, res) {
     Produto.find(function(err, prods) {
         if(err)
@@ -33,5 +33,57 @@ router.get('/', function(req, res) {
         });
     });
 });
+
+// GetById=> localhost:3000/api/produtos/{id}
+router.get('/:productId', function(req, res) {
+    const id = req.params.productId;
+
+    Produto.findById(id, function(err, produto) {
+        if(err){
+            res.status(500).json({
+                message: "Erro ao tentar encontrar produto; ID mal formado"
+            });
+        } else if(produto == null){
+            res.status(400).json({
+                message: "Produto não encontrado para o ID passado"
+            });
+        } else {
+            res.status(200).json({
+                message: "Produto encontrado",
+                produto: produto
+            });
+        }
+    });
+});
+
+// Put=> localhost:3000/api/produtos/{id}
+router.put('/:productId', function(req, res) {
+    const id = req.params.productId;
+    Produto.findById(id, function(err, produto) {
+        if(err){
+            res.status(500).json({
+                message: "Erro ao tentar encontrar o produto; Id mal formado"
+            });
+        } else if(produto == null) {
+            res.status(400).json({
+                message: "Produto não encontrado para o ID passado"
+            });
+        } else {
+            produto.nome = req.body.nome;
+            produto.preco = req.body.preco;
+            produto.descricao = req.body.descricao;
+
+            produto.save(function(error) {
+                if(error)
+                    res.send("Erro ao tentar atualizar o produto", error);
+
+                res.status(200).json({
+                    message: "Produto atualizado com sucesso"
+                });
+            });
+        }
+    });
+});
+
 
 module.exports = router;
